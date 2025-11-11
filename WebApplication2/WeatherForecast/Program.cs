35b68542d3dbac8e3165337d4922fedbb182e95f
Petter Astrup <petter.astrup@emmasys.com>
2025-11-11 09:51:44 +0100

    Add tests

--- WebApplication2/WeatherForecast/Program.cs
+++ WebApplication2/WeatherForecast/Program.cs
@@ -35,7 +35,9 @@ app.MapGet("/weatherforecast", () =>
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int TemperatureF => 32 + (int)Math.Floor(TemperatureC / 0.5556);
 }
+
+public partial class Program { }
